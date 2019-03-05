1890802424f2766430bc62148a75ff51ebd5de94
James Morgan <ja.morgan1@outlook.com>
2019-03-05 15:33:21 +1100

    Roxygenize

--- R/minutes.R
+++ R/minutes.R
@@ -1,3 +1,8 @@
+#' Knit R Markdown to Minutes PDF
+#'
+#' Render document as a PDF using the custom .tex file
+#' @param keep_tex Is TRUE by default. Set FALSE to discard intermediate .tex file during knit
+
 minutes <- function(..., keep_tex = TRUE){
 
     template <- find_template("minutes.tex")
--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,10 @@
+#' A Helper function used for locating a LaTeX template file.
+#'
+#' Locates the custom .tex template file in the rmarkdown/templates directory
+#' @param template_file Name of target template file
+
 find_template <- function(template_file){
-    template_file <- system.file("rmarkdown", "templates", template_file),
+    template_file <- system.file("rmarkdown", "templates", template_file,
                             package = "minutR")
     if(template_file == ""){
         stop("Couldn't locate template file ", template_file, call. = FALSE)
